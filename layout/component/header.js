61cf2c7d4385ffa783efebded13f9a5b0d612934
langnang <langnang.chen@outlook.com>
2021-07-05 16:55:02 +0800

    layout

--- layout/component/header.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-(function (define) {
-  define(function (require, exports, module) {
-    text = require("text!https://langnang.github.io/layout/component/header.hbs");
-    module.exports = text;
-  });
-})(define);

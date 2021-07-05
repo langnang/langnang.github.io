9714cfb89ce6815cd9f473875c562774ec041584
langnang <langnang.chen@outlook.com>
2021-07-05 16:41:33 +0800

    layout

---
+++ layout/component/footer.js
@@ -0,0 +1,7 @@
+"use strict";
+(function (define) {
+  define(function (require, exports, module) {
+    text = require("text!/layout/component/footer.hbs");
+    module.exports = text;
+  });
+})(define);
---
+++ layout/component/header.js
@@ -0,0 +1,7 @@
+"use strict";
+(function (define) {
+  define(function (require, exports, module) {
+    text = require("text!/layout/component/header.hbs");
+    module.exports = text;
+  });
+})(define);

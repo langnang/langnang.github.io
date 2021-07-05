29a3576b90e155a04259867a1d9c3fd30e1c974a
langnang <langnang.chen@outlook.com>
2021-07-05 17:15:40 +0800

    main.css

--- main.js
+++ main.js
@@ -11,6 +11,7 @@
       `text!${config.publicPath}/layout/component/header.hbs`,
       `text!${config.publicPath}/layout/component/footer.hbs`,
       "bootstrap",
+      "css!assets/css/main.css",
     ], function ($, Handlebars, headerHtml, footerHtml) {
       $("body>header").html(Handlebars.compile(headerHtml)(config));
       $("body>footer").html(Handlebars.compile(footerHtml)(config));

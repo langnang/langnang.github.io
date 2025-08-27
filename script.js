4f25f8693190c36e7a9024aef3e21c03a898a345
langnang <langnang.chen@outlook.com>
2025-08-27 17:57:55 +0800

    2025-08-27 17:57:55

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+(function ($, window, document, undefined) { })
+{
 
-$(function () {
-    $.fn.setCurrentTime = function (el, format = "hh:mm:ss") {
-        console.log(el);
+    $.fn.setCurrentTime = function (format = "hh:mm:ss") {
+        var date = new Date();
+
+        // 年、月、日
+        var year = date.getFullYear();
+        var month = ("00" + (date.getMonth() + 1)).slice(-2);
+        var day = ("00" + date.getDate()).slice(-2);
+
+        // 时、分、秒
+        var hour = ("00" + date.getHours()).slice(-2);
+        var minute = ("00" + date.getMinutes()).slice(-2);
+        var second = ("00" + date.getSeconds()).slice(-2);
+
+        // 实时显示
+        // element.innerText = "" + year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+        this.text(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second);
+        // console.log(this);
+        // console.log($(this));
+        // console.log(el);
     }
     $.fn.setCurrentData = function () { }
-})
+
+} (jQuery, window, document)
